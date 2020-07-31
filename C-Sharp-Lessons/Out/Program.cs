381e8838934cf6de8e899eb5bf20d84d2c451ea3
maxim218 <31542700+maxim218@users.noreply.github.com>
2020-07-31 22:45:30 +0300

    Out
    Out

---
+++ C-Sharp-Lessons/Out/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class ControllerDivMod
+{
+    private int a, b;
+
+    public ControllerDivMod(int aaa, int bbb)
+    {
+        a = aaa;
+        b = bbb;
+    }
+
+    public void getDivMod(out int divValue, out int modValue)
+    {
+        divValue = a / b;
+        modValue = a % b;
+    }
+}
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            ControllerDivMod controller = new ControllerDivMod(a, b);
+            int dd;
+            int mm;
+            controller.getDivMod(out dd, out mm);
+
+            Console.WriteLine(dd);
+            Console.WriteLine(mm);
+        }
+    }
+}
