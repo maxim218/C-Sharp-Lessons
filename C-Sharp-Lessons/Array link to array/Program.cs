4de5632dee8e686e2b414d9a6dd0acbcb2bc56be
maxim218 <31542700+maxim218@users.noreply.github.com>
2020-08-03 19:54:34 +0300

    Link array to array
    Link array to array

---
+++ C-Sharp-Lessons/Array link to array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++) Console.WriteLine(i + ") " + arr[i]);
+        }
+
+        static void Main(string[] args)
+        {
+            // создаем две ссылки на массивы
+            // изначально ссылки указывают в никуда
+            int[] first = null;
+            int[] second = null;
+
+            // теперь first ссылается на массив
+            // задаем значения массива
+            first = new int[4];
+            first[0] = 10;
+            first[1] = 20;
+            first[2] = 30;
+            first[3] = 40;
+
+            // выводим элементы массива на экран
+            printArray(first);
+
+            // отчеркиваем вывод
+            Console.WriteLine("-----------------------------");
+
+            // теперь second ссылается на тот же массив
+            second = first;
+
+            // меняем некоторые ячейки массива
+            second[1] = 2000;
+            second[3] = 4000;
+
+            // выводим элементы измененного массива на экран
+            printArray(first);
+        }
+    }
+}
