c2f685b4a964d36880a6d35b811d3dfc18146501
maxim218 <maxim218@yandex.ru>
2020-08-13 22:57:32 +0300

    Delegates

---
+++ C-Sharp-Lessons/Delegates as params/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Calculator
+{
+    public static int substractElems(int a, int b)
+    {
+        Console.WriteLine($"Substract elements: {a} {b}");
+        int sub = a - b;
+        return sub;
+    }
+
+    public static int multiplyElems(int x, int y)
+    {
+        Console.WriteLine($"Multiply elements: {x} {y}");
+        int mul = x * y;
+        return mul;
+    }
+}
+
+namespace MyProject
+{
+    class Program
+    {
+        delegate int MyFunc(int first, int second);
+
+        static int calculateAndRender(MyFunc myFunc, int firstParam, int secondParam)
+        {
+            int result = myFunc(firstParam, secondParam);
+            return result;
+        }
+       
+
+        static void Main(string[] args)
+        {
+            int f = 14;
+            int s = 11;
+
+            int sub = calculateAndRender(Calculator.substractElems, f, s);
+            int mul = calculateAndRender(Calculator.multiplyElems, f, s);
+
+            Console.WriteLine("Sub: " + sub);
+            Console.WriteLine("Mul: " + mul);
+        }
+    }
+}
---
+++ C-Sharp-Lessons/Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Calculator
+{
+    public static int substractElems(int a, int b)
+    {
+        Console.WriteLine($"Substract elements: {a} {b}");
+        int sub = a - b;
+        return sub;
+    }
+
+    public static int multiplyElems(int x, int y)
+    {
+        Console.WriteLine($"Multiply elements: {x} {y}");
+        int mul = x * y;
+        return mul;
+    }
+}
+
+namespace MyProject
+{
+    class Program
+    {
+        delegate int MyFunc(int first, int second);
+
+        static void Main(string[] args)
+        {
+            MyFunc myFunc;
+
+            myFunc = Calculator.substractElems;
+            int sub = myFunc(12, 8);
+            Console.WriteLine(sub);
+
+            myFunc = Calculator.multiplyElems;
+            int mul = myFunc(15, 6);
+            Console.WriteLine(mul);
+        }
+    }
+}
