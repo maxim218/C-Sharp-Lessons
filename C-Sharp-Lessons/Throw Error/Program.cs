78115d5be982bd06418f573c75d01559e88d2455
maxim218 <maxim218@yandex.ru>
2020-08-13 20:41:33 +0300

    Exeptions

---
+++ C-Sharp-Lessons/Throw Error/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (a < 0) throw new Exception("my error A");
+                if (b < 0) throw new Exception("my error B");
+                if (c < 0) throw new Exception("my error C");
+                int volume = a * b * c;
+                Console.WriteLine("Volume: " + volume);
+            } 
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                Console.WriteLine("Error: " + message);
+            }
+
+            Console.WriteLine("Finish program");
+        }
+    }
+}
---
+++ C-Sharp-Lessons/Try Catch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            try
+            {
+                int x = a / b;
+                Console.WriteLine("Result: " + x);
+            } 
+            catch
+            {
+                Console.WriteLine("Error of zero");
+            }
+
+            Console.WriteLine("Finish program");
+        }
+    }
+}
