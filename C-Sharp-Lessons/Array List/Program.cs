ea7717912780857f95e618c4554780aeb4d12453
maxim218 <maxim218@yandex.ru>
2020-08-15 17:44:03 +0300

    Collections

---
+++ C-Sharp-Lessons/Array List/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+class Man
+{
+    private string login;
+    private int age;
+
+    public Man(string login, int age)
+    {
+        this.login = login;
+        this.age = age;
+    }
+
+    public void mulAge()
+    {
+        this.age = this.age * 100;
+    }
+
+    public void renderMan()
+    {
+        string message = this.login + " " + this.age;
+        Console.WriteLine(message);
+    }
+}
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man nina = new Man("Nina", 45);
+            Man alex = new Man("Alex", 12);
+            Man george = new Man("George", 78);
+            Man maxim = new Man("Maxim", 37);
+
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(nina);
+            arrayList.Add(alex);
+            arrayList.Add(george);
+            arrayList.Add(maxim);
+
+            int count = arrayList.Count;
+            Console.WriteLine("Count: " + count + "\n");
+
+            Console.WriteLine("---------------------------------------\n");
+
+            for(int i = 0; i < count; i++)
+            {
+                Man man = (Man) arrayList[i];
+                man.renderMan();
+            }
+
+            Console.WriteLine("\n---------------------------------------\n");
+
+            for (int i = 0; i < count; i++)
+            {
+                Man man = (Man) arrayList[i];
+                man.mulAge();
+            }
+
+            nina.renderMan();
+            alex.renderMan();
+            george.renderMan();
+            maxim.renderMan();
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
---
+++ C-Sharp-Lessons/Dictionary/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Point
+{
+    private int x;
+    private int y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void renderPoint()
+    {
+        string message = " " + this.x + " " + this.y + "\n";
+        Console.Write(message);
+    }
+
+    public void changeFields(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <string, Point> dictionary = new Dictionary <string, Point> ();
+
+            dictionary["Moscow"] = new Point(100, 200);
+            dictionary["Berlin"] = new Point(1234, 5678);
+            dictionary["Samara"] = new Point(25, 43);
+
+            foreach(KeyValuePair <string, Point> keyValue in dictionary)
+            {
+                string key = keyValue.Key;
+                Point value = keyValue.Value;
+                Console.Write(key);
+                value.renderPoint();
+            }
+
+            Console.WriteLine("---------------------------------------");
+
+            try
+            {
+                string k = Console.ReadLine();
+                Point v = dictionary[k];
+                Console.Write(k);
+                v.renderPoint();
+            }
+            catch
+            {
+                Console.WriteLine("Element not found");
+            }
+
+            Console.WriteLine("---------------------------------------");
+
+            Point b = dictionary["Berlin"];
+            b.changeFields(111111, 2222222);
+
+            foreach (KeyValuePair<string, Point> keyValue in dictionary)
+            {
+                string key = keyValue.Key;
+                Point value = keyValue.Value;
+                Console.Write(key);
+                value.renderPoint();
+            }
+
+            Console.WriteLine("---------------------------------------");
+        }
+    }
+}
