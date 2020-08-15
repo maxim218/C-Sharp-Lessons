78c66dd888195c2b36412c5abee24000b9bd6b80
maxim218 <maxim218@yandex.ru>
2020-08-15 21:55:16 +0300

    Strings

---
+++ C-Sharp-Lessons/String Builder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("Hello ");
+            stringBuilder.Append("beautiful ");
+            stringBuilder.Append("gorgeous ");
+            stringBuilder.Append("world ");
+
+            string message = stringBuilder.ToString();
+            Console.WriteLine(message);
+        }
+    }
+}
