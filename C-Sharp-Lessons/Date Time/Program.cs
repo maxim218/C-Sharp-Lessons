94cf14e58a8fd639c27d32603e6ffe03058d4aaa
maxim218 <maxim218@yandex.ru>
2020-08-18 21:38:26 +0300

    Date

---
+++ C-Sharp-Lessons/Date Time/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // получаем дату и время в формате строки
+            string s = DateTime.Now.ToString();
+            Console.WriteLine(s);
+
+            Console.WriteLine("------------------------------------");
+
+            // модифицирую строку в собственный формат
+            s = s.Replace(" ", "_");
+            s = s.Replace(".", "_");
+            s = s.Replace(",", "_");
+            s = s.Replace(":", "_");
+            Console.WriteLine(s);
+        }
+    }
+}
