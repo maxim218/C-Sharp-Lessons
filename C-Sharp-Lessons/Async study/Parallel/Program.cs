b51594537bc2391a41026e52f7b53b021d7b0946
maxim218 <maxim218@yandex.ru>
2020-08-25 18:16:08 +0300

    Parallel

---
+++ C-Sharp-Lessons/Async study/Parallel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void myWaitAndPrint(string messageValue, int waitValue)
+            {
+                Thread.Sleep(waitValue);
+                Console.WriteLine(messageValue);
+            }
+
+            Console.WriteLine("--------------------------");
+
+            // запускаем задачи параллельно
+            Task tA = Task.Run(() => myWaitAndPrint("A", 7000));
+            Task tB = Task.Run(() => myWaitAndPrint("B", 4000));
+            Task tC = Task.Run(() => myWaitAndPrint("C", 2000));
+
+            // ждем завершения выполнения задач
+            tA.Wait();
+            tB.Wait();
+            tC.Wait();
+
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
