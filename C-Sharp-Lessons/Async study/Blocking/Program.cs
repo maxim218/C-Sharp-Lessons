78f2d9da1abcdeb959808c5ea2fc9168d739c6b0
maxim218 <maxim218@yandex.ru>
2020-08-25 17:47:26 +0300

    Blocking

---
+++ C-Sharp-Lessons/Async study/Blocking/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void myWaitAndPrint(string messageValue, int waitValue)
+            {
+                Thread.Sleep(waitValue);
+                Console.WriteLine(messageValue);
+            }
+
+            myWaitAndPrint("A", 7000);
+            myWaitAndPrint("B", 4000);
+            myWaitAndPrint("C", 2000);
+        }
+    }
+}
